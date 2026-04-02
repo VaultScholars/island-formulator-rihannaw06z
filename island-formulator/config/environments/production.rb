7dd53e25fc485edaebdd8048a4fa93adfd57bc84
rihannaw06z <ms.rihanna.wright06@gmail.com>
2026-04-02 18:50:53 -0500

    config prodyction file edit

--- island-formulator/config/environments/production.rb
+++ island-formulator/config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :digital_ocean
-
+  config.hosts << "rihannaw-island-formulator-sxki6.ondigitalocean.app" # Allow requests from the DigitalOcean Spaces endpoint
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # config.assume_ssl = true
 
